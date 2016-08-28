551f23ddbab43e8e1eaa39eaf0e163c7538d4f9d
Angelo <angelopez.design@gmail.com>
2016-08-28 18:35:39 -0500

    separar capas footer y header

---
+++ footer.php
@@ -0,0 +1,96 @@
+<footer>
+		<div class="row">
+			<div class="small-12 medium-4 columns paySection">
+				<div class="title title001">
+					<h4><span>¿Como Comprar?</span></h4>
+				</div>
+				<div class="small-12 columns cardHowPay">
+					<div class="small-3 columns cardHeadHowPay">
+						<p><span class="fi-price-tag"></span> </p>
+					</div>
+					<div class="small-9 columns cardBodyHowPay">
+						<h3>Pago</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+					</div>
+				</div>
+				<div class="small-12 columns cardHowPay">
+					<div class="small-3 columns cardHeadHowPay">
+						<p><span class="fi-torso-business"></span> </p>
+					</div>
+					<div class="small-9 columns cardBodyHowPay">
+						<h3>Legal</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+					</div>
+				</div>
+				<div class="small-12 columns cardHowPay">
+					<div class="small-3 columns cardHeadHowPay">
+						<p><span class="fi-shopping-bag"></span> </p>
+					</div>
+					<div class="small-9 columns cardBodyHowPay">
+						<h3>Envio</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+					</div>
+				</div>
+			</div>
+			<div class="small-12 medium-4 columns contactSection">
+				<div class="title title001">
+					<h4><span>Contacto</span></h4>
+				</div>
+				<div class="small-12 columns cardContactS">
+					<article class="logoDown">
+						<picture>
+		            		<source srcset="recursos/img/logo001.jpg" media="min-width 1200px">
+		            		<source srcset="recursos/img/logo001.jpg" media="min-width 768px">
+		            		<source srcset="recursos/img/logo001.jpg" media="min-width 320px">
+		            		<img src="recursos/img/logo001.jpg" alt="accesorios">
+		            	</picture>
+					</article>
+					<h4>CONTACTANOS</h4>
+					<p>Numero Movile</p>
+					<p><span class="fa fa-whatsapp"></span> <a href="" title="">3013577497</a></p>
+				</div>
+			</div>
+			<div class="small-12 medium-4 columns newSection">
+				<div class="title title001">
+					<h4><span>MÁS INFORMACIÓN</span></h4>
+				</div>
+				<div>
+					<form  accept-charset="utf-8" id="formDown">
+						<div class="contInput small-12 columns">
+							<input type="text" name="" id="" placeholder="Nombre completo">
+						</div>
+						<div class="contInput small-12 columns">
+							<input type="text" name="" id="" placeholder="Correo electronico">
+						</div>
+						<div class="contInput small-12 columns">
+							<input type="text" name="" id="" placeholder="Numero de contacto">
+						</div>
+						<div class="contInput small-12 columns">
+							<textarea name="" id="" placeholder="Mensaje"></textarea>
+						</div>
+						<div class="contInput small-12 columns">
+							<button type="button">ENVIAR</button>
+						</div>
+					</form>
+				</div>
+			</div>
+			<section class="small-12 columns copyRigth">
+				<p><i>Copyrigth Clocks Tienda Online 2016</i></p>
+			</section>
+		</div>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="js/swiper.min.js" type="text/javascript" charset="utf-8"></script>
+	<script>
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+    </script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Clocks Tienda</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<link rel="stylesheet" href="css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+	<link rel="stylesheet" type="text/css" href="icons/foundation-icons.css">
+	<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<style>
+	.swiper-container {
+		width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+    }
+    .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+        /* Center slide text vertically */
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
+    }
+	</style>
+</head>
+<body>
+	<section id="redesTop">
+		<div class="row">
+			<article>
+				<ul>
+					<li class=""><a href="" title=""><i><span class="fi-social-twitter"></i></a></li>
+					<li class=""><a href="" title=""><i><span class="fi-social-facebook"></span></i></a></li>
+					<li class=""><a href="" title=""><i><span class="fi-social-instagram"></i></a></li>
+					<li> Contacto: <a href="">301 357 74 97</a></li>
+					<li> Email:<a href=""> emailclock@clock.com</a></li>
+				</ul>
+			</article>
+		</div>
+	</section>
+	<header id="header" class="">
+		<div class="row">
+			<div class="thumbPage">
+				<img src="recursos/img/logo001.jpg" alt="logo">
+			</div>
+		</div>
+	</header><!-- /header -->
